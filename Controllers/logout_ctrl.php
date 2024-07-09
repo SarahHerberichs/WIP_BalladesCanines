e7b71909bc6bde3aa09340e9de62bfea4e70cc38
Herberichs <94360739+SarahHerberichs@users.noreply.github.com>
2024-07-09 09:45:01 +0200

    Update logout_ctrl.php

--- Controllers/logout_ctrl.php
+++ Controllers/logout_ctrl.php
@@ -1,11 +1,5 @@
 <?php
 // Détruit toutes les variables de session
 $_SESSION = array();
-echo('coucou ctrl');
-// Détruit la session côté serveur
-// session_destroy();
 
-// // Redirige vers la page d'accueil par exemple
-// header("?page=home"); // Assurez-vous de rediriger vers la bonne page
-// exit();
 ?>
