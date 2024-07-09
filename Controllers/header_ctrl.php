1963447c908ca768519b58423f31328fb592d24f
Herberichs <94360739+SarahHerberichs@users.noreply.github.com>
2024-07-09 09:44:35 +0200

    Update header_ctrl.php

--- Controllers/header_ctrl.php
+++ Controllers/header_ctrl.php
@@ -5,12 +5,10 @@ session_start();
 
 if (isset ($_POST['logout'])) {
 
-// Détruit toutes les variables de session
 $_SESSION = array();
 
 session_destroy();
 
-// Redirige vers la page d'accueil par exemple
 header('Location: ../index.php');
 exit();
 }
